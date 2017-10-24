5b2668ea1b7d97c89e30aa2cf44482ba67d57ad2
zhangchicheng <zhangchicheng@navercorp.com>
2017-10-24 13:05:24 +0800

    register python 2.7

--- DbUtil.py
+++ DbUtil.py
@@ -1,4 +1,5 @@
 # coding=UTF-8
+import MySQLdb
 
 def saveToDatabase(title, price, url, district_name, house_structure, house_size, house_orient, house_decoration,
                    house_elevator, unitPrice, img_url, follower_num, visit_num, release_time, building_info, area_name,
