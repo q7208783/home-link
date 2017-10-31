fb4e302b14432a71cc7062774107427d09a9824b
zhangchicheng <zhangchicheng@navercorp.com>
2017-10-31 16:58:52 +0800

    add logger

--- SearchNewHouese.py
+++ SearchNewHouese.py
@@ -3,8 +3,10 @@ import sys
 import time
 import platform
 
-import requests;
+import requests
 from lxml import html
+import logging
+import logging.handlers
 
 from MyQueue import *
 from SqureMap import squreDict
@@ -12,7 +14,8 @@ from model.House import House
 from utils import DbUtil, ReUtils
 
 url = "https://cd.lianjia.com/ershoufang/squre/co32ng1hu1nb1ba65ea10000ep10000/"
-
+LOG_FILE_WIN = "D:\home-link.log"
+LOG_FILE_LINUX = "/home/zhangcc/logs/python_log/home-link.log"
 reload(sys)  # 2
 sys.setdefaultencoding('utf-8')
 DbUtil.testdb()
@@ -21,12 +24,21 @@ queues = [];
 queuesDict = {}
 
 os_name = platform.platform()
-
+if ('Linux' in os_name):
+    handler = logging.handlers.RotatingFileHandler(LOG_FILE_LINUX, maxBytes=1024 * 1024, backupCount=5)
+else:
+    handler = logging.handlers.RotatingFileHandler(LOG_FILE_WIN, maxBytes=1024 * 1024, backupCount=5)
+fmt = '%(asctime)s - %(filename)s:%(lineno)s - %(name)s - %(message)s'
+formatter = logging.Formatter(fmt)   # 实例化formatter
+handler.setFormatter(formatter)
+logger = logging.getLogger('home-link')
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
 
 def get_houselist(squre):
     page = requests.get(url.replace('squre', squre))
     tree = html.fromstring(page.content.decode('UTF-8'))
-    if('Linux' in os_name):
+    if ('Linux' in os_name):
         print '---linux platform---'
         return tree.xpath('/html/body/div[4]/div[1]/ul/li')
     else:
@@ -76,8 +88,10 @@ def squreAllHouse(squre):
         queue = MyQueue(40)
     for house in get_houselist(squre):
         houseObj = getHouse(house, squreDict[squre])
+        logger.debug('get a house, house title: ' + houseObj.title)
         if houseObj.url not in queue:
             queue.enqueue(houseObj.url)
+            logger.debug('save house, house title: '+houseObj.title)
             save(houseObj)
             if (queue.isfull()):
                 queue.dequeue()
