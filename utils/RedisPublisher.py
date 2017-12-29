ca7fa56e2ffde88b1f9f0fb5860924322afd3513
zhangchicheng <zhangchicheng@navercorp.com>
2017-12-29 13:17:10 +0800

    add redis

--- utils/RedisPublisher.py
+++ utils/RedisPublisher.py
@@ -1,6 +1,9 @@
 import redis
 
 import config
+from common.LogMgr import LogMgr
+
+logger_publisher = LogMgr('publisher.log')
 
 
 class RedisPublisher(object):
@@ -10,4 +13,5 @@ class RedisPublisher(object):
         self.strict_redis = redis.StrictRedis(connection_pool=self.pool)
 
     def publish(self, channel, msg):
+        logger_publisher.info('channel:' + channel + 'msg:' + msg)
         return self.strict_redis.publish(channel=channel, message=msg)
